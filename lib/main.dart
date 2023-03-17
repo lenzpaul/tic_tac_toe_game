9abb88b2869d6218ac266c769131ed09e0b51be2
Lenz Paul <bringbackthedog@gmail.com>
2023-03-16 21:11:42 -0400

    xs and os

--- lib/main.dart
+++ lib/main.dart
@@ -26,14 +26,21 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   bool player1turn = true;
 
+  /// X's and O's
+  List<String> values = List.filled(9, '');
+
   /// When tapped, if the cell is empty and it is player1's turn, put an X, if it
   /// is player2's turn put an O. If the cell is not empty, do nothing.
-  void _onTap() {
+  void _onTap(int index) {
     //
-  }
+    print("tapped $index");
 
-  // X's and O's
-  List<String> values = List.filled(9, '');
+    if (values[index].isEmpty) {
+      values[index] = player1turn ? 'X' : 'O';
+      player1turn = !player1turn;
+      setState(() {});
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -43,14 +50,17 @@ class _HomeState extends State<Home> {
         gridDelegate:
             const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3),
         itemBuilder: (context, index) {
-          return Container(
-            decoration: BoxDecoration(
-              border: Border.all(),
-            ),
-            child: Center(
-              child: Text(
-                values[index],
-                style: TextStyle(fontSize: 50),
+          return GestureDetector(
+            onTap: () => _onTap(index),
+            child: Container(
+              decoration: BoxDecoration(
+                border: Border.all(),
+              ),
+              child: Center(
+                child: Text(
+                  values[index],
+                  style: const TextStyle(fontSize: 50),
+                ),
               ),
             ),
           );
