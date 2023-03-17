2571963220978b30b6b4a9cc9d83f2361546784d
Lenz Paul <bringbackthedog@gmail.com>
2023-03-16 20:34:06 -0400

    +grid

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,40 @@
+import 'package:flutter/material.dart';
 
+void main() {
+  runApp(const TicTacToeApp());
+}
+
+/// A tic-tac-toe game.
+class TicTacToeApp extends StatelessWidget {
+  const TicTacToeApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: GridView.builder(
+        itemCount: 9,
+        gridDelegate:
+            const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3),
+        itemBuilder: (context, index) {
+          return Container();
+        },
+      ),
+    );
+  }
+}
